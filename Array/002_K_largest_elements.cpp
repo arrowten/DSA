ba758576e909360eb3551f98d165cac3999d2ed4
Ajay Wakodikar <ajaywakodikarsocial@gmail.com>
2024-10-05 12:26:14 +0530

    Array 002: K largest elements

---
+++ Array/002_K_largest_elements.cpp
@@ -0,0 +1,61 @@
+/*
+Given an integer array arr[] of N integers and an integer K. Your task is to return K largest elements in decreasing order.
+
+Examples:
+
+#Input 1: arr[] = [1, 23, 12, 9, 30, 2, 50], K= 3
+#Output 1: 50 30 23
+
+#Input 2: arr[] = [12, 5, 787, 1, 23], K= 2
+#Output 2: 787 23
+
+#Input 3: arr[] = [11, 5, 12, 9, 44, 17, 2], K= 2
+#Output 3: 44 17
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <gtest/gtest.h>
+
+auto k_largest_elements(const std::vector<int>& nums, int K) -> std::vector<int> {
+    if(K == 0) return {};
+
+    int size = nums.size();
+    std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap;
+    std::vector<int> result;
+
+    //Push the first K elements from the vector into the min heap
+    for(int i = 0; i < K; i++) {
+        min_heap.push(nums[i]);
+    }
+
+    //Pop the top element if it is smaller than the current element and push the current element into the min heap
+    for(int i = K; i < size; i++) {
+        if(nums[i] > min_heap.top()) {
+            min_heap.pop();
+            min_heap.push(nums[i]);
+        }
+    }
+
+    //Push the elements back into a vector
+    while(!min_heap.empty()) {
+        result.push_back(min_heap.top());
+        min_heap.pop();
+    }
+
+    //Sort it in decreasing order for the greatest element to be at the start
+    std::sort(result.begin(), result.end(), std::greater<>{});
+
+    return result;
+}
+
+TEST(K_largest_elements, handles_various_cases) {
+    std::vector<int> nums = {1, 23, 12, 9, 30, 2, 50}, nums2 {12, 5, 787, 1, 23}, nums3 = {11, 5, 12, 9, 44, 17, 2};
+    std::vector<int> result = k_largest_elements(nums, 3), result2 = k_largest_elements(nums2, 2), result3 = k_largest_elements(nums3, 2);
+
+    EXPECT_EQ(result, (std::vector<int>{50, 30, 23}));
+    EXPECT_EQ(result2, (std::vector<int>{787, 23}));
+    EXPECT_EQ(result3, (std::vector<int>{44, 17}));
+}
